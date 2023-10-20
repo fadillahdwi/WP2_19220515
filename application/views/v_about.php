55ddc5ebb2ad531288b743f37f9c7d93a66700b6
fadillah dwi <fadillahdwiamelia470@gmail.com>
2023-10-20 14:48:41 +0700

    menambahkan file application dan assets

---
+++ application/views/v_about.php
@@ -0,0 +1,31 @@
+<h1><?php echo $judul ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RentalBuku</title>
+</head>
+<body>
+<img width=220 height=250  src=" <?= base_url('assets/img/fadila.jpeg') ?> ">
+<section>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fadillah Dwi </li>
+        <li>Nama Belakang : Amelia</li>
+    </ul>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Tambun Utara</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>hobi</h4>
+    <ul type="square">
+        <li>Mendengarkan Musik</li>
+    </ul>
+</section>
+</body>
+</html>
